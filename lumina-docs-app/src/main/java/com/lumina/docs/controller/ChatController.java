e6c7dec34d9aeeaeefa6b3110a1654976cb0638e
Gastertablook <gastertablook@gmail.com>
2026-04-19 03:45:27 +0800

    应用层重构查询接口

--- lumina-docs-app/src/main/java/com/lumina/docs/controller/ChatController.java
+++ lumina-docs-app/src/main/java/com/lumina/docs/controller/ChatController.java
@@ -24,11 +24,10 @@ public class ChatController {
 
         // 模拟当前会话ID和驾驭约束 (比如强制只在某个知识库里搜)
         String sessionId = UUID.randomUUID().toString();
-        Map<String, Object> constraints = new HashMap<>();
-        constraints.put("indexName", indexName);
+        Map<String, Object> metadataFilters = new HashMap<>();
         // constraints.put("userId", "1001"); // 未来做权限隔离的伏笔
 
         // 一行代码，呼叫底层的 V8 引擎！
-        return luminaRagClient.chatStream(query, sessionId, constraints);
+        return luminaRagClient.chatStream(query, sessionId, indexName, metadataFilters);
     }
 }
